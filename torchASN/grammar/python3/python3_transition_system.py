1fdab7677abd8430c4e4db1224786c8808da76de
Justin <justinyao@gatech.edu>
2021-12-05 23:05:47 -0500

    Fixes bug in action tree parsing

--- torchASN/grammar/python3/python3_transition_system.py
+++ torchASN/grammar/python3/python3_transition_system.py
@@ -133,15 +133,15 @@ class Python3TransitionSystem(TransitionSystem):
         return ActionTree(action, field_nodes)
 
     def build_ast_from_actions(self, action_tree):
-        if action_tree.action is None: # TODO for now only
-            return None
-
         if isinstance(action_tree, list):
             # print(action_tree)
             if isinstance(action_tree[0].action, ReduceAction):
                 return []
             return [self.build_ast_from_actions(at) for at in action_tree]
         else:
+
+            if action_tree.action is None: # TODO for now only
+                return None
 
             # Case for ReduceAction
             if isinstance(action_tree.action, ReduceAction):
